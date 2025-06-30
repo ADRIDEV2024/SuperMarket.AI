6dbfd35441b32800ae0fcbb670ec71794daf6988
Adrian <adriangomis34@gmail.com>
2025-06-30 11:04:28 +0200

    Update: subiendo utilidades y configuraciones

---
+++ config.py
@@ -0,0 +1,11 @@
+
+OPENAI_API_KEY = "38287f8b-2c3e-4d1a-9b6c-0f5e7c8d9e0a"  
+BOT_NAME = "Supermarket.AI Assistant"
+PRODUCTS_DATA_PATH = "postgresql://user:password@localhost:5432/products_database.db"
+DEFAULT_SYSTEM_PROMPT = (
+    "You are a helpful AI assistant specialized in supermarket products. "
+    "Your task is to assist users in finding products, checking prices, and "
+    "providing information about promotions and availability."
+)
+
+
