b0f1685637ac77a0460d0275f666a760c53a4f35
Adrian <adriangomis34@gmail.com>
2025-07-01 11:06:25 +0200

    Upload: init file for gui directory

---
+++ src/gui/__init__.py
@@ -0,0 +1,13 @@
+from .main_app import main
+from .chat_interface import ChatInterface
+from .product_display import display_product_list, display_product_detail
+from .settings_panel import show_settings_panel
+
+__all__ = [
+    "main",
+    "ChatInterface",
+    "display_product_list",
+    "display_product_detail",
+    "show_settings_panel",
+]
+
